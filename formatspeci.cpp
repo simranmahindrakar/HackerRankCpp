060eb4f2bfef46f94060d9c9fd42b7ed00fabc99
simranmahindrakar <66198982+simranmahindrakar@users.noreply.github.com>
2020-07-03 19:09:35 +0530

    Create formatspeci.cpp

---
+++ formatspeci.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int n;
+    long t;
+    char a;
+    float f;
+    double d;
+    scanf("%d %ld %c %f %lf", &n, &t, &a, &f, &d);
+    printf("%d \n" , n);
+    printf("%ld \n" , t);
+    printf("%c \n" , a);
+    printf("%f \n" , f);
+    printf("%lf \n" , d);
+
+    return 0;
+}
