0ffcc23c1469db330fb88f9c3aad8fcce4b9d9f3
simranmahindrakar <66198982+simranmahindrakar@users.noreply.github.com>
2020-07-05 12:29:21 +0530

    Create array.cpp

---
+++ array.cpp
@@ -0,0 +1,21 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int n;
+    cin>>n;
+    int arr[10000];
+    for (int i=0 ; i<n ; i++) {
+        cin>>arr[i];
+    }
+    for(int j=n-1 ; j>=0 ; j-- )
+    {
+        cout<<arr[j]<<" ";
+      
+    }
+}
